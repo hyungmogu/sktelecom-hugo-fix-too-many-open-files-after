ba47d4f0d84557a6030db4a7ab1bfa5bd793ec5a
Hyungmo Gu <guhyungm7@gmail.com>
2022-05-29 15:03:54 -0600

    Add files

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if [[ "$OSTYPE" =~ ^darwin ]]; then
+  sudo launchctl limit maxfiles 65535 200000;
+  ulimit -n 65535;
+  sudo sysctl -w kern.maxfiles=100000;
+  sudo sysctl -w kern.maxfilesperproc=65535;
+fi
+
+hugo server;
